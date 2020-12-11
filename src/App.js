4cd29bcf4921244104297de51d4b889f75681f04
Chris Van Huffe <cvanhuffel@outlook.com>
2020-12-10 21:30:00 -0800

    User Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component} from 'react';
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'John';
-    const loading = false;
-    const showName = true;
-    
+
     return (
-      <div className="App">
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
-        
+      <div className='App'>
+        <Navbar />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
